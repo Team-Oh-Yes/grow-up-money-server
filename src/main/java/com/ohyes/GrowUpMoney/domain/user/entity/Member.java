9cceefde923d638b9017d1479dd572ce9ae66e82
redjungi <150891110+redjungi09@users.noreply.github.com>
2025-09-30 19:58:21 +0900

    feat: 회원가입 오류수정 및 로그인 구현

--- src/main/java/com/ohyes/GrowUpMoney/domain/user/entity/Member.java
+++ src/main/java/com/ohyes/GrowUpMoney/domain/user/entity/Member.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
@@ -28,22 +29,26 @@ public class Member {
     private Integer point_balance;
 
     @Column(nullable = false, columnDefinition = "boolean default true")
-    private Boolean is_active;
+    private Boolean is_active = true;
 
     @Column(nullable = false, columnDefinition = "VARCHAR(255) default 'user'")
     private String tier;
 
-    @Column(nullable = false)
-    private Date created_at;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime created_at;
 
     @Column(nullable = false)
-    private Date updated_at;
+    private LocalDateTime updated_at;
 
-    public void setUsername(String username) {
-        if (username.length()<= 3 ){
-            throw new IllegalArgumentException();
-        }
-        this.username = username;
+
+    @PrePersist
+    public void prePersist() {
+        if (this.is_active == null) this.is_active = true;
+        if (this.point_balance == null) this.point_balance = 0;
+        if (this.tier == null) this.tier = "user";
+        LocalDateTime now = LocalDateTime.now();
+        this.created_at = now;
+        this.updated_at = now;
     }
 
 
--- src/main/java/com/ohyes/GrowUpMoney/domain/user/repository/MemberRepository.java
+++ src/main/java/com/ohyes/GrowUpMoney/domain/user/repository/MemberRepository.java
@@ -10,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
