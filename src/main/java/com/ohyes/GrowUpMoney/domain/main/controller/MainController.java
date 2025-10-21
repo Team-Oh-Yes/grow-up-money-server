5d4187129c1b4a5d7ff4d1064dc96ad8a80a0057
junha0729 <junha0729@dgsw.hs.kr>
2025-10-21 23:10:42 +0900

    feat: 메인페이지 컨트롤러 개발

--- src/main/java/com/ohyes/GrowUpMoney/domain/main/controller/MainController.java
+++ src/main/java/com/ohyes/GrowUpMoney/domain/main/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.ohyes.GrowUpMoney.domain.main.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/main")
 public class MainController {
+
+    @GetMapping("/banners")
+    public String getBanners(){
+        return "banners image";
+    }
+
+    @GetMapping("intro")
+    public String getIntro(){
+        return "Grow Money introduction";
+    }
 
 }
