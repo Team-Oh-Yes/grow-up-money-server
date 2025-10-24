c9017209141705549626792cc8f55c5c6fbfa5ee
redjungi <150891110+redjungi09@users.noreply.github.com>
2025-10-24 23:59:15 +0900

    feat: SignUp 응답 ResponseEntity로 수정

--- src/main/java/com/ohyes/GrowUpMoney/domain/user/controller/MemberController.java
+++ src/main/java/com/ohyes/GrowUpMoney/domain/user/controller/MemberController.java
@@ -1,22 +1,17 @@
 package com.ohyes.GrowUpMoney.domain.user.controller;
 
-import com.ohyes.GrowUpMoney.domain.user.dto.LoginRequset;
+import com.ohyes.GrowUpMoney.domain.user.dto.LoginRequest;
 import com.ohyes.GrowUpMoney.domain.user.dto.LoginResponse;
-import com.ohyes.GrowUpMoney.domain.user.dto.MemberDto;
-import com.ohyes.GrowUpMoney.domain.user.entity.CustomUser;
+import com.ohyes.GrowUpMoney.domain.user.dto.SignUpRequest;
+import com.ohyes.GrowUpMoney.domain.user.dto.SignUpResponse;
 import com.ohyes.GrowUpMoney.domain.user.repository.MemberRepository;
 import com.ohyes.GrowUpMoney.domain.user.service.AuthService;
 import com.ohyes.GrowUpMoney.global.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Locale;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -33,14 +28,17 @@ public class MemberController {
 
     @PostMapping("/signup")
     @ResponseBody
-    public String signUp(@RequestBody MemberDto requset){
-        authService.signUp(requset);
-        return "회원가입 완료";
+    public ResponseEntity<SignUpResponse> signUp(@RequestBody SignUpRequest request){
+        SignUpResponse responseBody = authService.signUp(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(responseBody);
     }
 
     @PostMapping("/login")
     @ResponseBody
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequset request){
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request){
         LoginResponse responseBody = authService.login(
                 request.getUsername(),
                 request.getPassword()
---
+++ src/main/java/com/ohyes/GrowUpMoney/domain/user/dto/SignUpResponse.java
@@ -0,0 +1,17 @@
+package com.ohyes.GrowUpMoney.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpResponse {
+
+    private String message;
+    private boolean success;
+
+}
