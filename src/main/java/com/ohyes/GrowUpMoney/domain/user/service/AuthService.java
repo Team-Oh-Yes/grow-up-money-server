3976cc6b106baa3f384c05d24cfff8091a0ee8ef
redjungi <150891110+redjungi09@users.noreply.github.com>
2025-10-24 12:13:22 +0900

    refactor:AuthService안에 login추가

--- src/main/java/com/ohyes/GrowUpMoney/domain/user/service/AuthService.java
+++ src/main/java/com/ohyes/GrowUpMoney/domain/user/service/AuthService.java
@@ -1,18 +1,26 @@
 package com.ohyes.GrowUpMoney.domain.user.service;
 
+import com.ohyes.GrowUpMoney.domain.user.dto.LoginResponse;
 import com.ohyes.GrowUpMoney.domain.user.dto.MemberDto;
+import com.ohyes.GrowUpMoney.domain.user.entity.CustomUser;
 import com.ohyes.GrowUpMoney.domain.user.entity.MemberEntity;
 import com.ohyes.GrowUpMoney.domain.user.repository.MemberRepository;
+import com.ohyes.GrowUpMoney.global.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+public class AuthService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     public void signUp(MemberDto request) {
         if (request.getUsername().length() <= 3){
@@ -41,6 +49,33 @@ public class MemberService {
         member.setEmail(request.getEmail());
         memberRepository.save(member); // DB 저장
 
+    }
+
+    public LoginResponse login(String username, String password) {
+
+        // 1. 인증 토큰 생성
+        var authToken = new UsernamePasswordAuthenticationToken(username, password);
+
+        // 2. 인증 시도 (Spring Security가 검증)
+        var auth = authenticationManagerBuilder.getObject().authenticate(authToken);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        // 3. JWT 생성
+        String jwt = jwtUtil.createToken(auth);
+
+        // 4. CustomUser 정보 추출
+        String extractedUsername = null;
+        Object principal = auth.getPrincipal();
+
+        if (principal instanceof CustomUser customUser) {
+            extractedUsername = customUser.getUsername();
+        }
+
+        // 5. 응답 DTO 생성
+        return new LoginResponse(
+                "로그인에 성공했습니다.",
+                jwt,
+                extractedUsername
+        );
     }
 
 }
