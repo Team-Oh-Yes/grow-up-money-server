75d6d2e65bbedcbb57cf7de40b7f0709959a4f2a
junha0729 <junha0729@dgsw.hs.kr>
2025-11-10 23:27:32 +0900

    feat: Lesson entity developed

--- src/main/java/com/ohyes/GrowUpMoney/domain/roadmap/entity/Lesson.java
+++ src/main/java/com/ohyes/GrowUpMoney/domain/roadmap/entity/Lesson.java
@@ -1,16 +1,23 @@
 package com.ohyes.GrowUpMoney.domain.roadmap.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tb_lesson")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Lesson {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "lesson_id")
     private Long id;
 
     // Theme: lesson, 1: N
@@ -18,14 +25,33 @@ public class Lesson {
     @JoinColumn(name = "theme_id", nullable = false)
     private Theme theme;
 
-    @Column(nullable = false, columnDefinition = "VARCHAR(100)")
+    @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(name = "order_index", nullable = false)
     private Integer orderIndex;
 
     @CreationTimestamp
-    @Column(nullable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @Builder
+    public Lesson(Theme theme, String title, Integer orderIndex) {
+        this.theme = theme;
+        this.title = title;
+        this.orderIndex = orderIndex;
+    }
+
+    public void updatetitle(String title) {
+        this.title = title;
+    }
+
+    public void updateOrderIndex(Integer orderIndex) {
+        this.orderIndex = orderIndex;
+    }
+
+    protected void setTheme(Theme theme) {
+        this.theme = theme;
+    }
 
 }
