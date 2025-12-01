cda84dd277b9568e0965626c3346335ee0572ba8
junha0729 <junha0729@dgsw.hs.kr>
2025-12-01 22:58:34 +0900

    feat: profile response developed

---
+++ src/main/java/com/ohyes/GrowUpMoney/domain/mypage/dto/response/MyProfileResponse.java
@@ -0,0 +1,23 @@
+package com.ohyes.GrowUpMoney.domain.mypage.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyProfileResponse {
+
+    private Long userId;
+    private String username;
+    private String nickname;
+    private String email;
+    private String profileImageUrl;
+    private String introduction;
+    private Long favoriteNftId; // 대표 에쁘띠
+    private String favoriteNftName;
+    private String favoriteNftImageUrl;
+}
