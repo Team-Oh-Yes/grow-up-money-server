27a2ad9503fe72bddea810bc93ff3288efd096f2
redjungi <150891110+redjungi09@users.noreply.github.com>
2025-12-15 00:23:34 +0900

    feat: 프로필 이미지 등록 컨트롤러를 presignedUrl형식으로 변경

--- src/main/java/com/ohyes/GrowUpMoney/domain/member/controller/MyPageController.java
+++ src/main/java/com/ohyes/GrowUpMoney/domain/member/controller/MyPageController.java
@@ -1,6 +1,8 @@
 package com.ohyes.GrowUpMoney.domain.member.controller;
 
 import com.ohyes.GrowUpMoney.domain.auth.entity.CustomUser;
+import com.ohyes.GrowUpMoney.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,9 +10,15 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Map;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/my")
+@RequiredArgsConstructor
 public class MyPageController {
+
+    private final MemberService memberService;
 
     @GetMapping("/profile")
     public ResponseEntity<?>getProfile(){
@@ -22,13 +30,34 @@ public class MyPageController {
         return ResponseEntity.ok(200);
     }
 
-    @PostMapping("/profile/image")
-    public ResponseEntity<?> uploadProfileImage(
+    //presignedUrl발급
+    @GetMapping("/profile/image/presigned-url")
+    public ResponseEntity<Map<String, String>> getPresignedUrl(
             @AuthenticationPrincipal CustomUser user,
-            @RequestParam("file")MultipartFile file
-            ){
-        String username  = user.getUsername();
-        //서비스 호출
-        return ResponseEntity.ok(200);
+            @RequestParam String fileName) {
+
+        String username = user.getUsername();
+        String key = "profiles/" + username + "/" + UUID.randomUUID() + "-" + fileName;
+        String presignedUrl = s3Service.generatePresignedUploadUrl(key);
+
+        return ResponseEntity.ok(Map.of(
+                "presignedUrl", presignedUrl,
+                "key", key
+        ));
     }
+
+    //DB에 경로저장
+    @PostMapping("/profile/image")
+    public ResponseEntity<?> saveProfileImage(
+            @AuthenticationPrincipal CustomUser user,
+            @RequestBody Map<String, String> request) {
+
+        String username = user.getUsername();
+        String imageKey = request.get("key");
+
+        memberService.updateProfileImage(username, imageKey);
+
+        return ResponseEntity.ok(Map.of("message", "프로필 이미지 업로드 완료"));
+    }
+
 }
