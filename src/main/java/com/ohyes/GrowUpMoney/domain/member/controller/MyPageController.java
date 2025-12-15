d843673cc14b4d9f230b06673b59dd1d8115b279
redjungi <150891110+redjungi09@users.noreply.github.com>
2025-12-15 10:34:53 +0900

    feat: 프로필 수정기능 개발

--- src/main/java/com/ohyes/GrowUpMoney/domain/member/controller/MyPageController.java
+++ src/main/java/com/ohyes/GrowUpMoney/domain/member/controller/MyPageController.java
@@ -1,19 +1,20 @@
 package com.ohyes.GrowUpMoney.domain.member.controller;
 
 import com.ohyes.GrowUpMoney.domain.auth.entity.CustomUser;
+import com.ohyes.GrowUpMoney.domain.member.dto.request.ProfileRequest;
 import com.ohyes.GrowUpMoney.domain.member.dto.response.PresignedUrlResponse;
+import com.ohyes.GrowUpMoney.domain.member.dto.response.ProfileResponse;
+import com.ohyes.GrowUpMoney.domain.member.entity.Member;
+import com.ohyes.GrowUpMoney.domain.member.repository.MemberRepository;
 import com.ohyes.GrowUpMoney.domain.member.service.MemberService;
 import com.ohyes.GrowUpMoney.domain.member.service.S3Service;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/my")
@@ -21,11 +22,35 @@ import java.util.UUID;
 public class MyPageController {
 
     private final MemberService memberService;
+    private final MemberRepository memberRepository;
     private final S3Service s3Service;
 
     @GetMapping("/profile")
-    public ResponseEntity<?>getProfile(){
-        return ResponseEntity.ok(200);
+    public ResponseEntity<ProfileResponse>getProfile(
+            @AuthenticationPrincipal CustomUser user
+    ){
+        String username = user.getUsername();
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(()-> new UsernameNotFoundException("존재하지 않는 사용자 입니다"));
+
+        return ResponseEntity.ok(ProfileResponse.builder()
+                .username(username)
+                .introduction(member.getIntroduction())
+                .profileImageUrl(member.getProfileImageUrl())
+                .favoriteNftId(member.getFavoriteNftId())//추후에 id가아닌 nft이미지로 변경예정
+                .build());
+    }
+
+    @PostMapping("/profile")
+    public ResponseEntity<Map<String,String>>updateProfile(
+            @AuthenticationPrincipal CustomUser user,
+            @RequestBody ProfileRequest request
+    ){
+        memberService.updateProfile(user, request);
+
+        return ResponseEntity.ok(Map.of(
+                "message", "수정이 완료되었습니다."
+        ));
     }
 
     @GetMapping("/statistics")
